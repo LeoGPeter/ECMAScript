2f3ff09ce1d0b7595644b9cede21bb2baee830a7
Leonardo Peter <leopeter98@hotmail.com>
2024-01-10 18:49:06 -0300

    Actualizacion de datos

--- fileSystem.js
+++ fileSystem.js
@@ -27,7 +27,7 @@ class ProductManager {
 
     readProducts = async () => {
        let respuesta = await fs.readFile(this.patch, "utf-8")
-        return JSON.parse(respuesta);
+        return JSON.parse(respuesta)
     }
 
     getProducts = async () => {
@@ -37,6 +37,7 @@ class ProductManager {
 
     getProductsById = async (id) => {
         let respuesta3 = await this.readProducts();
+        id = parseInt(id);
         if(!respuesta3.find((product) => product.id === id)){
             console.log("Producto no encontrado")
         }else{
@@ -44,40 +45,43 @@ class ProductManager {
         }
     };
 
-    deleteProductsById = async () => {
-        let respuesta3 = await this.readProducts();
-        let productfilter = respuesta3.filter((products) => products.id != id);
-        await fs.writeFile(this.patch, JSON.stringify(productfilter));
-        console.log("Producto eliminado");
+    deleteProductsById = async (id) => {
+            let respuesta3 = await this.readProducts();
+            let productfilter = respuesta3.filter(products => products.id != id);
+            await fs.writeFile(this.patch, JSON.stringify(productfilter));
+            console.log("Producto eliminado");
     };
 
-    updateProducts = async ({id, ...producto}) => {
-        await this.deleteProductsById(id);
-        let productOld = await this.readProducts();
-        let productomodificado = [{...producto, id}, ...productOld];
-        await fs.writeFile(this.patch, JSON.stringify(productomodificado));
+    updateProduct = async ({id, ...updatedFields}) => {
+        let products = await this.readProducts();
+        const index = products.findIndex(product => product.id === id);
+    
+        if (index !== -1) {
+            products[index] = { ...products[index], ...updatedFields };
+            await fs.writeFile(this.patch, JSON.stringify(products));
+            console.log("Producto actualizado");
+        } else {
+            console.log("Producto no encontrado");
+        }
     };
 }
 
 const productos = new ProductManager();
 
-// productos.addProduct("titutlo1", "descripcion1", 2500, "img1", "asd123", 3);
-// productos.addProduct("titutlo2", "descripcion2", 4000, "img2", "asd456", 8);
-// productos.addProduct("titutlo3", "descripcion3", 5500, "img3", "asd678",6);
+//  productos.addProduct("titutlo1", "descripcion1", 2500, "img1", "asd123", 3);
+//  productos.addProduct("titutlo2", "descripcion2", 4000, "img2", "asd456", 8);
+//  productos.addProduct("titutlo3", "descripcion3", 5500, "img3", "asd678",6);
 
-// productos.getProducts()
+//  productos.getProducts()
 
-// productos.getProductsById(4)
+//  productos.getProductsById(4)
 
-// productos.deleteProductsById(1)
+//  productos.deleteProductsById(1)
 
-productos.updateProducts({
-    title: 'titutlo1',
-  description: 'descripcion1',
-  price: 6000,
-  img: 'img1',
-  code: 'asd123',
-  stock: 3,
-  id: 1
+productos.updateProduct({
+  price: 9800,
+  stock: 15,
+  id:1
+  
 })
 
